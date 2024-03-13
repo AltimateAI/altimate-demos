a29ac685ac15ee75ac97374042882c66f457093e
Patricia Goldberg <patricia.goldberg@park-depot.de>
2024-03-13 22:38:21 +0100

    Change the source to BigQuery

--- models/marts/users.sql
+++ models/marts/users.sql
@@ -1,38 +1,58 @@
 with
-    salesforce_users as (
-        select
-            user_id,
-            email,
-            first_name,
-            last_name,
-            phone,
-            address,
-            is_active,
-            last_contacted_date,
-            created_date,
-            modified_date
-        from {{ ref("base_salesforce_users") }}
+    salesforce_users as (select * from {{ ref("base_salesforce_users") }}),
+    postgres_users as (select * from {{ ref("base_public_user") }}),
+    activities as (select * from {{ ref("base_public_activity") }}),
+
+    activity_by_user as (
+        select user_id, count(*) as number_of_activities
+        from activities
+        group by user_id
     ),
-    postgres_users as (
-        select user_id, username, created_at from {{ ref("base_public_user") }}
+    users_info as (
+        select
+            salesforce_users.user_id,
+            salesforce_users.email,
+
+            postgres_users.username,
+            salesforce_users.first_name,
+            salesforce_users.last_name,
+            salesforce_users.phone,
+            salesforce_users.address,
+
+            salesforce_users.is_active,
+
+            salesforce_users.last_contacted_date,
+            salesforce_users.created_date,
+            salesforce_users.modified_date,
+
+            postgres_users.created_at
+        from salesforce_users
+        left join postgres_users using (user_id)
+    ),
+
+    final as (
+        select
+            users_info.user_id,
+            users_info.email,
+
+            users_info.username,
+            users_info.first_name,
+            users_info.last_name,
+            users_info.phone,
+            users_info.address,
+
+            activity_by_user.number_of_activities,
+
+            users_info.is_active,
+
+            users_info.last_contacted_date,
+            users_info.created_date,
+            users_info.modified_date,
+
+            users_info.created_at
+        from users_info
+        left join activity_by_user using (user_id)
     )
 
-select
-    salesforce_users.user_id,
-    salesforce_users.email,
-
-    postgres_users.username,
-    salesforce_users.first_name,
-    salesforce_users.last_name,
-    salesforce_users.phone,
-    salesforce_users.address,
-
-    salesforce_users.is_active,
-
-    salesforce_users.last_contacted_date,
-    salesforce_users.created_date,
-    salesforce_users.modified_date,
-
-    postgres_users.created_at
-from salesforce_users
-left join postgres_users on salesforce_users.user_id = postgres_users.user_id
+select *
+from final
