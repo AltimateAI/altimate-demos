4b981e396467fc8c265905743720c49ffb141532
Patricia Goldberg <patricia.goldberg@park-depot.de>
2024-03-29 22:15:27 +0100

    Topic Data Quality tests

---
+++ models/marts/user_activities.sql
@@ -0,0 +1,19 @@
+with users as (
+    select * from {{ ref('users') }}
+), activities as (
+    select * from {{ ref('base_public_activity') }}
+),
+
+final as (
+    select
+        users.email,
+        activities.activity_type,
+
+        count(*) as number_of_activities,
+        max(activity_date) as last_activity_date
+    from users
+    left join activities using (user_id)
+    group by email, activity_type
+)
+
+select * from final
