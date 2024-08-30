ccc1a67c1a98952b8e7db038c69f06904d789f49
Patricia Goldberg <patricia.goldberg@park-depot.de>
2024-08-30 18:22:30 +0200

    Start clean branch

--- models/marts/daily_users.sql
+++
@@ -1,9 +0,0 @@
-with salesforce_users as (
-    select * from {{ ref("base_salesforce_users") }}
-), postgres_user as (
-    select * from {{ ref('base_public_user') }}
-)
-
-select *
-from salesforce_users
-left join postgres_user using (user_id, email)
