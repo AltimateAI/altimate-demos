20af6b60e67dbf9fa0822955632a2ed66d85553c
Patricia Goldberg <patricia.goldberg@park-depot.de>
2024-10-05 15:58:48 +0200

    users update

--- models/marts/users.sql
+++ models/marts/users.sql
@@ -6,19 +6,24 @@ salesforce_users as (select * from {{ ref('stg_salesforce__user') }}),
 user_activities as (
     select
         user_id,
-        count(*) as count_activity
+        count(*) as count_activity,
+        count(case when activity_type = 'login' then 1 end) as count_login,
+        count(case when activity_type = 'logout' then 1 end) as count_logout,
+        count(case when activity_type = 'post' then 1 end) as count_post
     from postgres_activity
     group by user_id
 ),
 
-final as (
-    select
+final AS (
+    SELECT
         user_id,
-
-        user_activities.count_activity
-    from postgres_users
-    inner join salesforce_users using (user_id)
-    left join user_activities using (user_id)
+        user_activities.count_activity,
+        user_activities.count_login,
+        user_activities.count_logout,
+        user_activities.count_post
+    FROM postgres_users
+    INNER JOIN salesforce_users USING (user_id)
+    LEFT JOIN user_activities USING (user_id)
 )
 
 select *
